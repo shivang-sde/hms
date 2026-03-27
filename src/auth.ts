cb20bae1b0094a8695395527a6aeb8b7cf998f1f
shivang-tripati <shivangtripathi101@gmail.com>
2026-03-27 16:02:01 +0530

    fix: traefik & host issue

--- src/auth.ts
+++ src/auth.ts
@@ -8,7 +8,6 @@ import { UserRole } from "@prisma/client";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
     ...authConfig,
-    trustHost: true,
     providers: [
         Credentials({
             async authorize(credentials) {
